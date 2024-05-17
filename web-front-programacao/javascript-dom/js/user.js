3dcc75a8c9b417fbb54e9af507967aa1a0235a0a
Giovanni <giovanni_ge@hotmail.com>
2024-05-17 16:10:54 -0300

    feat: adicionando projeto

--- web-front-programacao/javascript-dom/js/user.js
+++ web-front-programacao/javascript-dom/js/user.js
@@ -2,9 +2,18 @@ const textUser = document.querySelector("#text-user");
 const buttonUser = document.querySelector("#screen-name");
 const result = document.querySelector(".result");
 
+function clearInput() {
+  textUser.value = "";
+}
+
 function cadUser(event) {
   event.preventDefault();
+
+  // validações
+  if (textUser.value.trim() === '') {
+    alert("Campo obrigatório");  
+    return false;
+  }
   
   result.innerHTML += `<li> Bem vindo, ${textUser.value} </li>`;
-  textUser.value = "";
 }
