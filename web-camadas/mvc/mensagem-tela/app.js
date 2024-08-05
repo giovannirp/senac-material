43e3c2eff3ad9cea86c5e1aa6c07e12551d16717
Giovanni <giovanni_ge@hotmail.com>
2024-08-05 16:22:11 -0300

    feat: adicionando layout

--- web-camadas/mvc/mensagem-tela/app.js
+++ web-camadas/mvc/mensagem-tela/app.js
@@ -1,20 +1,20 @@
 // Model: Dados
 const model = {
-    message: 'Olá, você clicou no botão!'
-  };
-  
-  // View: Atualiza a interface
-  const view = {
-    button: document.querySelector('#myButton'),
-    messageParagraph: document.querySelector('#message'),
-    
-    // Mostra a mensagem na tela
-    showMessage() {
-      this.messageParagraph.textContent = model.message;
-    }
-  };
-  
-  // Controller: Lógica de interação
-  view.button.addEventListener('click', () => {
-    view.showMessage();
-  });
+  message: "Olá, você clicou no botão!",
+};
+
+// View: Atualiza a interface
+const view = {
+  button: document.querySelector("#myButton"),
+  messageParagraph: document.querySelector("#message"),
+
+  // Mostra a mensagem na tela
+  showMessage() {
+    this.messageParagraph.textContent = model.message;
+  },
+};
+
+// Controller: Lógica de interação
+view.button.addEventListener("click", () => {
+  view.showMessage();
+});
