74001ffd7bc1d8f3df770c07ccf4eccf0f1c59cb
Giovanni <giovanni_ge@hotmail.com>
2024-08-06 11:26:13 -0300

    feat: adicionando

---
+++ desenvolvimento-api-rest/primeira-aula/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+// indicar para express ler body com json
+app.use(express.json());
+
+// mock
+const nomes = [
+  { id: 1, nome: "Fernanda", idade: "18" },
+  { id: 2, nome: "Caio", idade: "23" },
+  { id: 3, nome: "Pedro", idade: "56" },
+  { id: 4, nome: "Samuel", idade: "45" },
+];
+
+app.get("/", function (req, res) {
+  res.send("Olá Mundo!");
+});
+
+// Buscando todos os nomes
+app.get("/listaNomes", (req, res) => {
+  res.status(200).send(nomes);
+});
+
+// Criando Função Auxiliar
+function buscarNomePorId(id) {
+    return nomes.filter((nome) => nome.id == id);
+}
+
+// Buscando por ID
+app.get('/listaNomes/:id' , (req, res) => {
+    let index = req.params.id;
+
+    res.json(buscarNomePorId(index));
+});
+
+app.listen(3000);
