54cae2526c831eb985e8fe4213eefdefde73b532
Giovanni <giovanni_ge@hotmail.com>
2024-08-28 14:30:59 -0300

    feat: add funcao auxiliar

--- desenvolvimento-api-rest/web-04-inicial/src/app.js
+++ desenvolvimento-api-rest/web-04-inicial/src/app.js
@@ -4,6 +4,17 @@ const app = express();
 // indicar para express ler body com json
 app.use(express.json());
 
+// função auxiliar
+// retornar o objeto por id
+function buscarSelecaoPorId(id) {
+  return selecoes.filter((selecao) => selecao.id == id);
+}
+
+// pegar a posicao ou index do elemento do array por id
+function buscarIdSelecao(id) {
+  return selecoes.findIndex(selecao => selecao.id == id);
+}
+
 app.get("/", function (req, res) {
     res.send("Testando API");
   });
