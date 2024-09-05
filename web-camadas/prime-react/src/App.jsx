d6983c07f1dee1a0957f857ea76295c341e648be
giovanni <giovanni_ge@hotmai.com>
2024-09-04 21:16:31 -0300

    feate

--- web-camadas/prime-react/src/App.jsx
+++ web-camadas/prime-react/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-principal">Seja bem vindo!</h1>
         <Router />
       </div>
     </>
--- web-camadas/prime-react/src/Router.jsx
+++ web-camadas/prime-react/src/Router.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Contato from "./Pages/Contato";
+import Pessoas from "./Pages/Pessoas";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contato" element={<Contato />} />
+        <Route path="/pessoas" element={<Pessoas />} />
       </Routes>
     </BrowserRouter>
   )
