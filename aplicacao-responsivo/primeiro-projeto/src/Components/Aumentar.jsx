a403241276317b7e692c43a497b21e4b321c46aa
Giovanni <giovanni_ge@hotmail.com>
2024-10-02 11:04:58 -0300

    feat: add

--- aplicacao-responsivo/primeiro-projeto/src/Components/Aumentar.jsx
+++ aplicacao-responsivo/primeiro-projeto/src/Components/Aumentar.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 
 const Aumentar = () => {
   // Inicializa o estado com o valor 0
-  const [statue, setStatue] = useState(0);
+  const [count, setCount] = useState(0);
 
   // Função para aumentar o valor do estado
   const aumentar = () => {
-    setStatue(statue + 1);
+    setCount(count + 1);
   };
 
     // Função para diminuir o valor do estado
     const diminuir = () => {
-      setStatue(statue - 1);
+        setCount(count - 1);
     };
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>{statue}</h1>
+      <h1>{count}</h1>
       <button onClick={aumentar} style={{ marginRight: '10px' }}>Aumentar Statue</button>
       <button onClick={diminuir} >Diminuir</button>
     </div>
